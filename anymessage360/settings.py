b9b9850ee8c57c7f971f332d387acf7057d09293
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-09-20 22:11:15 -0400

    feat: se agrega aplicacion servicios y modelo inicial

--- anymessage360/settings.py
+++ anymessage360/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'servicios', # se debe agregar la aplicacion para que django reconozca los modelos
 ]
 
 MIDDLEWARE = [
