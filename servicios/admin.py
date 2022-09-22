79a5f120c96f3c6eba0faf98bb2d2d1c5cfd651c
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-09-21 22:17:34 -0400

    feat: se complementa modelos y se agregan a administrador

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# importacion de modelos de la aplicacion
+from .models import Persona
+from .models import Usuario
+
+#registro de modelos para visualizar en el administrador
+admin.site.register(Persona)
+admin.site.register(Usuario)
--- servicios/models.py
+++ servicios/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
-class Cliente(models.Model):
+class Persona(models.Model):
   nombre = models.CharField(max_length=100)
-  
+
+  def __str__(self):
+    return self.nombre
+
+class Usuario(models.Model):
+  user = models.CharField(max_length=30, unique=True)
+  password = models.CharField(max_length=128)
+  enviar_correo = models.BooleanField(default=False)
+  precio_correo = models.DecimalField(max_digits=10, decimal_places=2)
+  persona = models.ForeignKey(Persona, on_delete=models.CASCADE)
+  # persona = models.ForeignKey('self', on_delete=models.CASCADE, related_name='Persona')
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
